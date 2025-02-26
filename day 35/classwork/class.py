99e422dd4722b4c1a112497613af5315d3bc6bc7
lukinooo777 <lukaxurcidze381@gmail.com>
2025-02-26 17:57:09 +0400

    second commit

---
+++ day 35/classwork/class.py
@@ -0,0 +1,11 @@
+bill_amount = float(input())
+
+tape =bill_amount + 0.2
+
+print(tape)
+
+
+def find_time(distance, speed):
+    time = distance/speedprint(f"(time) hours")
+
+    find_time(7425,550)
