8b3dd0daf0f38257018031eb0f6c94f5823f3f86
lukinooo777 <lukaxurcidze381@gmail.com>
2025-04-28 22:53:02 +0400

    second commit

---
+++ day 57/classwork/script.js
@@ -0,0 +1,9 @@
+let x = promt("Enter your name")
+let y = promt("enter your  lastName")
+let city = promt("Enter your city")
+let z =promt("Enter your Proffesion")
+
+
+let totalItems = 200;
+let itemsPerBox = 7;
+let remainingItems = totalItems % itemsPerBox;
---
+++ day 57/homework/scrit.js
@@ -0,0 +1,39 @@
+let a = 5;
+a++;
+console.log(a); 
+
+let b = 10;
+b++;
+console.log(b); 
+
+let c = 0;
+c++;
+console.log(c);
+
+let da = -3;
+d++;
+console.log(d); 
+
+let ea = 99;
+e++;
+console.log(e); 
+
+let aa = 5;
+console.log(a++); 
+console.log(a);   
+
+let ba = 10;
+console.log(b--); 
+console.log(b);   
+
+let c a= 0;
+console.log(c++); 
+console.log(c);   
+
+let d = 3;
+console.log(d--); 
+console.log(d);   
+
+let e = -1;
+console.log(e++); 
+console.log(e);  
