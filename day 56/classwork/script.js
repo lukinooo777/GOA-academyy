67a227753e2d188e72fea37255f7844d3262db4b
lukinooo777 <lukaxurcidze381@gmail.com>
2025-04-28 22:32:51 +0400

    second commit

---
+++ day 56/classwork/script.js
@@ -0,0 +1,8 @@
+let name = promt("Enter your name:")
+let lastName = promt("Enter your lastName");
+
+let age = Number(promt("Enter your age"));
+
+let city = promt("Enter your city");
+
+console.log("Hellow My name is" + name + "my lastname is" + lastName + "i am " + age + );
