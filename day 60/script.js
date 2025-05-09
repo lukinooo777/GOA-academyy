eb9ba6689e46f2065f57ed5feb7af054bad87336
lukinooo777 <lukaxurcidze381@gmail.com>
2025-05-09 16:17:06 +0400

    second commit

---
+++ day 60/script.js
@@ -0,0 +1,69 @@
+switch (dayNumber) {
+    case 1:
+        alert("ორშაბათი");
+        break;
+    case 2:
+        alert("სამშაბათი");
+        break;
+    case 3:
+        alert("ოთხშაბათი");
+        break;
+    case 4:
+        alert("ხუთშაბათი");
+        break;
+    case 5:
+        alert("პარასკევი");
+        break;
+    case 6:
+        alert("შაბათი");
+        break;
+    case 7:
+        alert("კვირა");
+        break;
+    default:
+        alert("არასწორი დღე");
+        break;
+}
+
+
+switch (monthNumber) {
+    case 1:
+        monthName = "იანვარი";
+        break;
+    case 2:
+        monthName = "თებერვალი";
+        break;
+    case 3:
+        monthName = "მარტი";
+        break;
+    case 4:
+        monthName = "აპრილი";
+        break;
+    case 5:
+        monthName = "მაისი";
+        break;
+    case 6:
+        monthName = "ივნისი";
+        break;
+    case 7:
+        monthName = "ივლისი";
+        break;
+    case 8:
+        monthName = "აგვისტო";
+        break;
+    case 9:
+        monthName = "სექტემბერი";
+        break;
+    case 10:
+        monthName = "ოქტომბერი";
+        break;
+    case 11:
+        monthName = "ნოემბერი";
+        break;
+    case 12:
+        monthName = "დეკემბერი";
+        break;
+    default:
+        monthName = null;
+}
+
