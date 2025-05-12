859194423b1d09ae555e3bd4dd84d6d1deaf4cbc
lukinooo777 <lukaxurcidze381@gmail.com>
2025-05-12 23:46:51 +0400

    second commit

---
+++ day 61/classwork/script.js
@@ -0,0 +1,16 @@
+for(let i = 2 ; i <= 50; i += 2){
+    console.log(i)
+}
+
+
+for(let i = 1; i <= 199; i += 2){
+    console.log(i);
+}
+
+for(let i = 2; i <= 200; i += 2){
+    console.log(i);
+}
+
+for(let i = 0; i < 3; i++){
+    console.log("GOA IS THE BEST")
+}
