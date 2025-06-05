9a64902a423e907146dd45149bbbdc24bbe6bfd3
lukinooo777 <lukaxurcidze381@gmail.com>
2025-06-06 01:07:14 +0400

    second commit

---
+++ day 65/classwork/script.js
@@ -0,0 +1,10 @@
+const me ={
+    name: "luka" ,
+    age: 14,
+    proffesion = "developer" ,
+    hobby: "programm" ,
+
+     introduce: function() {
+        return "hello. l am ${this.name}", ${this.age} ${this.proffesion} this ${.hobby}
+     }
+}
