3cec83a3ba0bca48abf1cf2b6d6e745782484c48
lukinooo777 <lukaxurcidze381@gmail.com>
2025-06-22 04:15:41 +0400

    second commit

---
+++ day 72/classwork/script.js
@@ -0,0 +1,9 @@
+function loveCalculator(name1, name2) {
+  const percentage = Math.floor(Math.random() * 100) + 1;
+
+
+  alert(${name1}  ${name2}, ${percentage});
+}
+
+
+loveCalculator("ანა", "ნიკა");
