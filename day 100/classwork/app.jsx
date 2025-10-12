069a60c2b4b20ec7f8109b64066633384ea5d0f8
lukinooo777 <lukaxurcidze381@gmail.com>
2025-10-12 16:21:23 +0400

    second commit

---
+++ day 100/classwork/app.jsx
@@ -0,0 +1,23 @@
+let icnrease = () => {
+   console.log(increase)    
+};
+
+
+let discrease = () => {
+   console.log(discrease)
+};
+
+return(
+
+<div>
+
+    <img src="" alt="k2" />
+    <h1>Hellow world</h1>
+
+    <span>(counter)</span>
+    <button onclick={increase}>up</button>
+     <button onclick={discrease}>down</button>
+</div>
+
+
+)
