1d51ea98fbd219efbc04ac5c1c952325bc15a9d9
lukinooo777 <lukaxurcidze381@gmail.com>
2025-11-11 00:49:50 +0400

    second commit

---
+++ day 101/classwork/app.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from React + Vite 🚀</h1>
+      <button onClick={() => alert('You clicked Button 1!')}>Button 1</button>
+      <button onClick={() => alert('You clicked Button 2!')}>Button 2</button>
+      <button onClick={() => alert('You clicked Button 3!')}>Button 3</button>
+    </div>
+  );
+}
+
+export default App;
