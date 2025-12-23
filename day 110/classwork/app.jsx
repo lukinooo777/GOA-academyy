abb559d85191bc19e8cd8705ce1eb84683a98793
lukinooo777 <lukaxurcidze381@gmail.com>
2025-12-23 21:08:27 +0400

    second commit

---
+++ day 110/classwork/app.jsx
@@ -0,0 +1,15 @@
+
+const name = "Luka";
+<h1>{name}</h1>
+
+const age = 15;
+<p>{age}</p>
+
+const isStudent = true;
+<span>{isStudent}</span>
+
+const city = "Tbilisi";
+<div>{city}</div>
+
+const score = 100;
+<h2>{score}</h2>
